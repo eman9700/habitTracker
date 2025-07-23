e8684a229e411c9fa61ce44662144e8fd4239776
emantheman <99277011+eman9700@users.noreply.github.com>
2025-07-22 23:52:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Firebase App Hosting Backend!'));
+app.get('/', (req, res) => res.send('Hello Irving, how are you doing?'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
